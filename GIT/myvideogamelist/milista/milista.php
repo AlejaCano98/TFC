b542301613f0a31560302f630756ce13008a193b
AlejaCano98 <60430421+AlejaCano98@users.noreply.github.com>
2021-11-14 13:15:40 +0100

    TERCERA ACTUALIZACION DEL TFC 14/11/21

---
+++ GIT/myvideogamelist/milista/milista.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">  
+    <link rel="stylesheet" type="text/css" href="../main.css">
+    <link rel="shortcut icon" type="image/jpg" href="../img/favicon.ico"/> 
+    <title> Myvideogamelist | Perfil </title>
+</head>
+<body>
+        <div class="nav-contenido">
+        <header>
+                <a  class="inicio" href="../index.php">Myvideogamelist</a>
+            <nav>
+                <ul>
+                    <li><a href="../juegos/juegos.php">Busca videojuegos</a></li>
+                    <li><a href="acceso.php">Acceso</a></li>
+                </ul>
+            </nav>
+        </header>
+</html>
